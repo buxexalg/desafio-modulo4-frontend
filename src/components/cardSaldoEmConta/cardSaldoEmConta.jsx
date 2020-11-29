9911ed8425a24379ebfe7ce1466e906e183338e0
buxexalg <lucasgralcantara@gmail.com>
2020-11-29 13:41:52 -0300

    Funcionalidade: Card de saldo em conta

---
+++ src/components/cardSaldoEmConta/cardSaldoEmConta.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import iconeSaldo from '../../assets/images/saldo.png';
+
+import './styles.css';
+
+export function CardSaldoEmConta(props) {
+	const [saldo, setSaldo] = React.useState('0');
+
+	return (
+		<div className="saldoEmConta">
+			<ul>
+				<li>
+					<img src={iconeSaldo} alt="Ícone dinheiro" />
+					<span>Saldo em conta</span>
+				</li>
+				<li>{'R$' + saldo * 100}</li>
+			</ul>
+		</div>
+	);
+}
