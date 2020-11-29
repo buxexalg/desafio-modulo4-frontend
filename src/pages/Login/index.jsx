04a9fdc2663cda3a2c9fe49ad37c102711a70d0a
buxexalg <lucasgralcantara@gmail.com>
2020-11-29 10:18:49 -0300

    Funcionalidade: Autenticação

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { Link, useLocation } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
 
 import { BotaoLogin } from '../../components/botaoLogin/botaoLogin';
 import { InputLogin } from '../../components/inputLogin/inputLogin';
-import { InputMostrarSenha } from '../../components/inputMostrarSenha/inputMostrarSenha';
+import { fazerRequisicaoComBody } from '../../utils/requisicoes';
 
 import logo from '../../assets/images/logo.png';
 import './styles.css';
@@ -11,62 +12,99 @@ import './styles.css';
 export function LoginPage(props) {
 	const currentPath = useLocation().pathname;
 	const [senhaExposta, setSenhaExposta] = React.useState(false);
+	const { token, setToken } = props;
+
+	const { register, handleSubmit } = useForm();
 
 	return (
 		<div className="main">
 			<div className="areaLogin">
 				<img src={logo} alt="Logo Academy" />
 				{currentPath === '/' && (
-					<>
+					<form
+						onSubmit={handleSubmit(async (dados) => {
+							console.log(dados);
+							fazerRequisicaoComBody(
+								'https://cubos-desafio-4.herokuapp.com/auth',
+								'POST',
+								dados
+							)
+								.then((response) => response.json())
+								.then((responseJson) => {
+									setToken(responseJson.dados.token);
+								});
+						})}
+					>
 						<InputLogin
 							label="E-mail"
 							type="email"
 							name="email"
-							placeholder="exemplo@gmail.com"
+							placeholder="exemplo@email.com"
+							register={register}
 						/>
 						<InputLogin
 							label="Senha"
 							type={!senhaExposta ? 'password' : 'text'}
-							name="password"
+							name="senha"
 							placeholder={null}
 							senhaExposta={senhaExposta}
 							setSenhaExposta={setSenhaExposta}
+							register={register}
 						/>
 
 						<Link to="/recuperar-senha" className="esqueciSenha">
 							Esqueci minha senha
 						</Link>
 						<BotaoLogin conteudo="Entrar" />
-					</>
+					</form>
 				)}
 				{currentPath === '/cadastro' && (
-					<>
+					<form
+						onSubmit={handleSubmit(async (dados) => {
+							fazerRequisicaoComBody(
+								'https://cubos-desafio-4.herokuapp.com/auth',
+								'POST',
+								dados
+							)
+								.then((response) => response.json())
+								.then((responseJson) => {
+									window.location.href = '/';
+								});
+						})}
+					>
 						<InputLogin
 							label="Nome"
 							type="text"
-							name="Nome"
+							name="nome"
 							placeholder={null}
+							register={register}
 						/>
 						<InputLogin
 							label="E-mail"
 							type="email"
 							name="email"
 							placeholder="exemplo@gmail.com"
+							register={register}
 						/>
 						<InputLogin
 							label="Senha"
 							type={!senhaExposta ? 'password' : 'text'}
-							name="password"
+							name="senha"
 							placeholder={null}
 							senhaExposta={senhaExposta}
 							setSenhaExposta={setSenhaExposta}
+							register={register}
 						/>
 
 						<BotaoLogin conteudo="Criar conta" />
-					</>
+					</form>
 				)}{' '}
 				{currentPath === '/recuperar-senha' && (
-					<>
+					<form
+						onSubmit={handleSubmit((dados) => {
+							console.log(dados);
+						})}
+					>
 						<p className="informacaoResetarSenha">
 							Informe o e-mail associado a sua conta e vamos te
 							enviar instruções para resetar sua senha
@@ -76,9 +114,10 @@ export function LoginPage(props) {
 							type="email"
 							name="email"
 							placeholder="exemplo@gmail.com"
+							register={register}
 						/>
 						<BotaoLogin conteudo="Recuperar senha" />
-					</>
+					</form>
 				)}
 			</div>
 
