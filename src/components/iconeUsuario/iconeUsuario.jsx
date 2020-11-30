63a81ccb4fa8cd59cc1c80e72825efbe2490dc87
buxexalg <lucasgralcantara@gmail.com>
2020-11-29 23:21:50 -0300

    Refatoração: Retirada do props

--- src/components/iconeUsuario/iconeUsuario.jsx
+++ src/components/iconeUsuario/iconeUsuario.jsx
@@ -6,15 +6,15 @@ import iconePerfil from '../../assets/images/iconePerfil.png';
 
 import { BotaoDeslogar } from '../botaoDeslogar/botaoDeslogar';
 
-export function IconeUsuario(props) {
+export function IconeUsuario() {
 	const [deslogar, setDeslogar] = React.useState(false);
 	return (
-		<>
+		<div className="user">
 			<button onClick={() => setDeslogar(!deslogar)}>
 				<img src={iconePerfil} alt="Foto de perfil do usuário" />
 			</button>
 
 			{deslogar && <BotaoDeslogar />}
-		</>
+		</div>
 	);
 }
