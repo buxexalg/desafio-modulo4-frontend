37c05c841c3d36189a7a84fe0fdc554cc0043ee9
buxexalg <lucasgralcantara@gmail.com>
2020-11-29 23:19:21 -0300

    Funcionalidade: Componente botão deslogar

--- src/components/botaoDeslogar/botaoDeslogar.jsx
+++ src/components/botaoDeslogar/botaoDeslogar.jsx
@@ -4,9 +4,9 @@ import './styles.css';
 
 import logout from '../../assets/images/log-out.png';
 
-export function BotaoDeslogar(props) {
+export function BotaoDeslogar() {
 	return (
-		<button>
+		<button className='botaoDeslogar'>
 			<img src={logout} alt="Logout" />
 			<span>Deslogar</span>
 		</button>
