bd655b54876e4de3b12215da3590e1534833a6bc
buxexalg <lucasgralcantara@gmail.com>
2020-12-01 23:34:21 -0300

    funcionalidade: página criar cobrança

--- src/pages/CriarCobranca/index.jsx
+++ src/pages/CriarCobranca/index.jsx
@@ -6,42 +6,115 @@ import { useForm } from 'react-hook-form';
 import './styles.css';
 import { ContextoToken } from '../../App';
 import { NavBar } from '../../components/navBar/navBar';
-import { InputLogin } from '../../components/inputLogin/inputLogin';
 import { BotaoLogin } from '../../components/botaoLogin/botaoLogin';
 
 export function CriarCobranca() {
 	const { token, setToken } = React.useContext(ContextoToken);
 
-	const { register, handleSubmit } = useForm();
+	const { register, handleSubmit, errors, trigger } = useForm({
+		mode: 'all',
+	});
+
+	const clienteInputRef = React.useRef();
+	const descricaoInputRef = React.useRef();
+	const valorInputRef = React.useRef();
+	const vencimentoInputRef = React.useRef();
+
+	React.useEffect(() => trigger(), [trigger]);
+
+	const qtdErros = Object.keys(errors).length;
+
 	return (
 		<div className="paginaCriarCobranca">
 			<NavBar />
 			<div className="conteudoCriarCobranca">
 				<h1>{'// CRIAR COBRANÇA'}</h1>
-				<div className="formCobranca">
-					<form>
-						<select name="Clientes" id="clientes">
-							<option value="a">a</option>
-							<option value="a">a</option>
-							<option value="a">a</option>
-							<option value="a">a</option>
-						</select>
-						<textarea
-							name="Descrição"
-							id="descricao"
-							cols="30"
-							rows="10"
-						>
-							Descrição
-						</textarea>
-						<span>
-							A descrição informada será impressa no boleto.
-						</span>
-						<InputLogin />
-						<InputLogin />
-						<div>
-							<BotaoLogin />
-							<BotaoLogin />
+				<div className="formCriarCobranca">
+					<form
+						onSubmit={handleSubmit(async (dados) => {
+							console.log(dados);
+						})}
+					>
+						<label>
+							<span> Cliente</span> <br />
+							<select
+								name="Clientes"
+								id="clientes"
+								className="selectCriarCobrancas"
+								ref={(element) => {
+									clienteInputRef.current = element;
+									register({
+										required: true,
+										validate: () => element.checkValidity(),
+									})(element);
+								}}
+							>
+								<option value="" disabled selected>
+									Selecione a cliente
+								</option>
+								<option value="a">a</option>
+								<option value="a">a</option>
+								<option value="a">a</option>
+							</select>
+						</label>
+						<label>
+							<span>Descrição</span> <br />
+							<textarea
+								name="Descrição"
+								id="descricao"
+								cols="30"
+								rows="10"
+								ref={(element) => {
+									descricaoInputRef.current = element;
+									register({
+										required: true,
+										validate: () => element.checkValidity(),
+									})(element);
+								}}
+							></textarea>
+							<br />
+							<span className="descricaoTextarea">
+								A descrição informada será impressa no boleto.
+							</span>
+						</label>
+						<div className="finzinho">
+							<label>
+								<span> Valor </span> <br />
+								<input
+									type="money"
+									name="valor"
+									ref={(element) => {
+										valorInputRef.current = element;
+										register({
+											required: true,
+											validate: () =>
+												element.checkValidity(),
+										})(element);
+									}}
+								/>
+							</label>
+							<label>
+								<span>Vencimento</span> <br />
+								<input
+									type="date"
+									name="vencimento"
+									ref={(element) => {
+										vencimentoInputRef.current = element;
+										register({
+											required: true,
+											validate: () =>
+												element.checkValidity(),
+										})(element);
+									}}
+								/>
+							</label>
+						</div>
+						<div className="botoesCriarCobranca">
+							<a href="/home">Cancelar</a>
+							<BotaoLogin
+								disable={qtdErros > 0}
+								conteudo="Criar cobrança"
+							/>
 						</div>
 					</form>
 				</div>
