22a01c4ee7811d354db57ffc398f80dfcc14655a
buxexalg <lucasgralcantara@gmail.com>
2020-12-04 03:04:32 -0300

    refatoração: tratamento de data

--- src/components/cardRelatorioCobrancas/cardRelatorioCobrancas.jsx
+++ src/components/cardRelatorioCobrancas/cardRelatorioCobrancas.jsx
@@ -7,17 +7,23 @@ import { StatusCobranca } from '../statusCobranca/statusCobranca';
 import './styles.css';
 
 export function CardRelatorioCobrancas(props) {
+	
+	const data = props.vencimento.split('T')[0];
+	const data0 = data.split('-').reverse().join('/');
 	return (
 		<tr className="CardRelatorioCobrancas">
 			<td>{props.cliente}</td>
 			<td>{props.descricao}</td>
 			<td>{'R$' + props.valor / 100}</td>
-			<StatusCobranca status={props.status} />
-			<td>{props.vencimento}</td>
+			<StatusCobranca
+				status={props.status}
+				idcobranca={props.idcobranca}
+			/>
+			<td>{data0}</td>
 			<td>
-				<button>
+				<a href={props.linkdoboleto}>
 					<img src={printer} alt="Gerar boleto" />
-				</button>
+				</a>
 			</td>
 		</tr>
 	);
