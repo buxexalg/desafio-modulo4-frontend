aaef381dcd57e57b73e2353fda3168f11ec20fbe
buxexalg <lucasgralcantara@gmail.com>
2020-12-04 03:03:45 -0300

    Funcionalidade: hook de login e logout

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './styles.css';
 
 import React from 'react';
+import PrivateRoute from './components/privateRoute/privateRoute';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import { LoginPage } from './pages/Login';
 import { HomePage } from './pages/Home';
@@ -9,16 +10,52 @@ import { Clientes } from './pages/Clientes';
 import { CriarCobranca } from './pages/CriarCobranca';
 import { CriarCliente } from './pages/CriarCliente';
 import { EditarCliente } from './pages/EditarCliente';
+import { createContainer } from 'unstated-next';
+import { fazerRequisicaoComBody } from './utils/requisicoes';
 
-export const ContextoToken = React.createContext();
+require('dotenv').config();
+
+function useLogin(estadoInicial = null) {
+	const apiURL = 'http://localhost:8081';
+	const [token, setToken] = React.useState(localStorage.getItem('token'));
+
+	React.useEffect(() => {
+		token
+			? localStorage.setItem('token', token)
+			: localStorage.removeItem('token');
+	}, [token]);
+
+	function login(email, senha) {
+		fazerRequisicaoComBody(apiURL + '/auth', 'POST', {
+			email,
+			senha,
+		})
+			.then((res) => res.json())
+			.then(({ dados }) => {
+				if (dados.token) {
+					setToken(dados.token);
+					window.location.href = '/home';
+				} else {
+					alert('Usuário e/ou senha incorretos.');
+				}
+			});
+	}
+
+	function logout() {
+		setToken(null);
+	}
+
+	return { token, login, logout, apiURL };
+}
+
+export const LoginContainer = createContainer(useLogin);
 
 export default function App() {
-	const [token, setToken] = React.useState(null); //useContext
 	return (
 		<BrowserRouter>
-			<div className="conteudo">
-				<Switch>
-					<ContextoToken.Provider value={(token, setToken)}>
+			<LoginContainer.Provider>
+				<div className="conteudo">
+					<Switch>
 						<Route
 							exact
 							path={['/', '/cadastro']}
@@ -42,10 +79,10 @@ export default function App() {
 							path="/editar-cliente"
 							component={EditarCliente}
 						/>
-						{/* <Route path="*" render={() => <h1>404</h1>} /> */}
-					</ContextoToken.Provider>
-				</Switch>
-			</div>
+						<Route exact path="*" component={() => <h1>404</h1>} />
+					</Switch>
+				</div>
+			</LoginContainer.Provider>
 		</BrowserRouter>
 	);
 }
