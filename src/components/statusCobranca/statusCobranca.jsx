43575b1c94fb644eaa57115c5c4d227348dfaaa2
buxexalg <lucasgralcantara@gmail.com>
2020-12-04 03:07:14 -0300

    funcionalidade: pagamento do boleto

--- src/components/statusCobranca/statusCobranca.jsx
+++ src/components/statusCobranca/statusCobranca.jsx
@@ -3,13 +3,25 @@ import React from 'react';
 import pendente from '../../assets/images/pendente.png';
 import pago from '../../assets/images/pago.png';
 
+import { fazerRequisicaoComBody } from '../../utils/requisicoes';
+import { LoginContainer } from '../../App';
 import './styles.css';
 
 export function StatusCobranca(props) {
+	const { token, apiURL } = LoginContainer.useContainer();
 	if (props.status === 'AGUARDANDO') {
 		return (
 			<td className="statusPendente">
-				<button>
+				<button
+					onClick={() => {
+						fazerRequisicaoComBody(
+							apiURL + '/cobrancas',
+							'PUT',
+							{ idDaCobranca: props.idcobranca },
+							token
+						);
+					}}
+				>
 					<img src={pendente} alt="Pendente" />
 				</button>
 				<span>PENDENTE</span>
@@ -17,12 +29,14 @@ export function StatusCobranca(props) {
 		);
 	} else if (props.status === 'PAGO') {
 		return (
-			<td className="statusPago">
-				<button>
-					<img src={pago} alt="Pago" />
-				</button>
-				<span>PAGO</span>
-			</td>
+			<>
+				<td className="statusPago">
+					<button>
+						<img src={pago} alt="Pago" />
+					</button>
+					<span>PAGO</span>
+				</td>
+			</>
 		);
 	} else if (props.status === 'VENCIDO') {
 		return (
