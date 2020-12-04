eebafbb4ad94932bc0e49fee2519de878becec52
buxexalg <lucasgralcantara@gmail.com>
2020-12-04 03:04:00 -0300

    funcionalidade: adicionada função de logout

--- src/components/botaoDeslogar/botaoDeslogar.jsx
+++ src/components/botaoDeslogar/botaoDeslogar.jsx
@@ -2,12 +2,15 @@ import React from 'react';
 
 import './styles.css';
 
-import logout from '../../assets/images/log-out.png';
+import logouticon from '../../assets/images/log-out.png';
+
+import { LoginContainer } from '../../App';
 
 export function BotaoDeslogar() {
+	const { logout, token } = LoginContainer.useContainer();
 	return (
-		<a href="/" className="botaoDeslogar">
-			<img src={logout} alt="Logout" />
+		<a href="/" className="botaoDeslogar" onClick={() => logout()}>
+			<img src={logouticon} alt="Logout" />
 			<span>Deslogar</span>
 		</a>
 	);
