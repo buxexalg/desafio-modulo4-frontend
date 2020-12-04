65b6f45f52b54c9a3869a4608c641d0b83daab2d
buxexalg <lucasgralcantara@gmail.com>
2020-12-04 03:04:47 -0300

    funcionalidade: recebe saldo em conta da API

--- src/components/cardSaldoEmConta/cardSaldoEmConta.jsx
+++ src/components/cardSaldoEmConta/cardSaldoEmConta.jsx
@@ -1,11 +1,32 @@
 import React from 'react';
 
 import iconeSaldo from '../../assets/images/saldo.png';
+import { LoginContainer } from '../../App';
 
 import './styles.css';
 
 export function CardSaldoEmConta() {
-	const [saldo, setSaldo] = React.useState('0');
+	const [saldo, setSaldo] = React.useState(0);
+
+	const { token, apiURL } = LoginContainer.useContainer();
+
+	React.useEffect(() => {
+		fetch(apiURL + '/relatorios', {
+			method: 'GET',
+			headers: {
+				Authorization: `Bearer ${token}`,
+			},
+		})
+			.then((response) => {
+				return response.json();
+			})
+			.then(({ dados }) => {
+				setSaldo(dados.relatorio.saldoEmConta);
+			})
+			.catch((err) => {
+				console.error(err);
+			});
+	}, [token]);
 
 	return (
 		<div className="saldoEmConta">
@@ -14,7 +35,7 @@ export function CardSaldoEmConta() {
 					<img src={iconeSaldo} alt="Ícone dinheiro" />
 					<span>Saldo em conta</span>
 				</li>
-				<li>{'R$' + saldo * 100}</li>
+				<li>{'R$  ' + (saldo / 100).toFixed(2)}</li>
 			</ul>
 		</div>
 	);
